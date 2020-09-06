fbec300042ab7eea1520257719116ea233c7be38
xavier Lembo <xlembo@eliberty.fr>
2020-09-06 12:16:15 +0200

    fix 3.4 compat

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -16,33 +16,45 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('redpill_polyfill_messenger')
             ->fixXmlConfig('transport')
-            ->fixXmlConfig('bus', 'buses');
+            ->fixXmlConfig('bus', 'buses')
+            ->validate()
+            ->ifTrue(function ($v) { return isset($v['buses']) && \count($v['buses']) > 1 && null === $v['default_bus']; })
+            ->thenInvalid('You must specify the "default_bus" if you define more than one bus.')
+            ->end()
+            ->validate()
+            ->ifTrue(static function ($v): bool { return isset($v['buses']) && null !== $v['default_bus'] && !isset($v['buses'][$v['default_bus']]); })
+            ->then(static function (array $v): void { throw new InvalidConfigurationException(sprintf('The specified default bus "%s" is not configured. Available buses are "%s".', $v['default_bus'], implode('", "', array_keys($v['buses'])))); })
+            ->end();
 
-        $rootNode->children()
+        $rootNode
+            ->children()
             ->arrayNode('routing')
+            ->normalizeKeys(false)
             ->useAttributeAsKey('message_class')
             ->beforeNormalization()
             ->always()
-            ->then(static function ($config): array {
+            ->then(function ($config) {
                 if (!\is_array($config)) {
                     return [];
+                }
+                // If XML config with only one routing attribute
+                if (2 === \count($config) && isset($config['message-class']) && isset($config['sender'])) {
+                    $config = [0 => $config];
                 }
 
                 $newConfig = [];
                 foreach ($config as $k => $v) {
                     if (!\is_int($k)) {
                         $newConfig[$k] = [
-                            'senders' => $v['senders'] ?? (\is_array($v) ? \array_values($v) : [$v]),
-                            'send_and_handle' => $v['send_and_handle'] ?? false,
+                            'senders' => $v['senders'] ?? (\is_array($v) ? array_values($v) : [$v]),
                         ];
                     } else {
-                        $newConfig[$v['message-class']]['senders'] = \array_map(
-                            static function ($a) {
+                        $newConfig[$v['message-class']]['senders'] = array_map(
+                            function ($a) {
                                 return \is_string($a) ? $a : $a['service'];
                             },
-                            \array_values($v['sender'])
+                            array_values($v['sender'])
                         );
-                        $newConfig[$v['message-class']]['send-and-handle'] = $v['send-and-handle'] ?? false;
                     }
                 }
 
@@ -50,67 +62,87 @@ final class Configuration implements ConfigurationInterface
             })
             ->end()
             ->prototype('array')
+            ->performNoDeepMerging()
             ->children()
             ->arrayNode('senders')
             ->requiresAtLeastOneElement()
             ->prototype('scalar')->end()
             ->end()
-            ->booleanNode('send_and_handle')->defaultFalse()->end()
             ->end()
             ->end()
             ->end()
             ->arrayNode('serializer')
             ->addDefaultsIfNotSet()
-            ->beforeNormalization()
-            ->always()
-            ->then(static function ($config): array {
-                if (false === $config) {
-                    return ['id' => null];
-                }
-
-                if (\is_string($config)) {
-                    return ['id' => $config];
-                }
-
-                return $config;
-            })
-            ->end()
             ->children()
-            ->scalarNode('id')->defaultValue(!\class_exists(FullStack::class) && \class_exists(Serializer::class) ? 'messenger.transport.symfony_serializer' : null)->end()
-            ->scalarNode('enabled')->defaultValue(\class_exists(Serializer::class))->end()
-            ->scalarNode('format')->defaultValue('json')->end()
+            ->scalarNode('default_serializer')
+            ->defaultValue('messenger.transport.native_php_serializer')
+            ->info('Service id to use as the default serializer for the transports.')
+            ->end()
+            ->arrayNode('symfony_serializer')
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('format')->defaultValue('json')->info('Serialization format for the messenger.transport.symfony_serializer service (which is not the serializer used by default).')->end()
             ->arrayNode('context')
             ->normalizeKeys(false)
             ->useAttributeAsKey('name')
             ->defaultValue([])
+            ->info('Context array for the messenger.transport.symfony_serializer service (which is not the serializer used by default).')
             ->prototype('variable')->end()
             ->end()
             ->end()
             ->end()
+            ->end()
+            ->end()
             ->arrayNode('transports')
+            ->normalizeKeys(false)
             ->useAttributeAsKey('name')
             ->arrayPrototype()
             ->beforeNormalization()
             ->ifString()
-            ->then(static function (string $dsn): array {
+            ->then(function (string $dsn) {
                 return ['dsn' => $dsn];
             })
             ->end()
             ->fixXmlConfig('option')
             ->children()
             ->scalarNode('dsn')->end()
+            ->scalarNode('serializer')->defaultNull()->info('Service id of a custom serializer to use.')->end()
             ->arrayNode('options')
             ->normalizeKeys(false)
             ->defaultValue([])
             ->prototype('variable')
             ->end()
             ->end()
+            ->arrayNode('retry_strategy')
+            ->addDefaultsIfNotSet()
+            ->beforeNormalization()
+            ->always(function ($v) {
+                if (isset($v['service']) && (isset($v['max_retries']) || isset($v['delay']) || isset($v['multiplier']) || isset($v['max_delay']))) {
+                    throw new \InvalidArgumentException('The "service" cannot be used along with the other "retry_strategy" options.');
+                }
+
+                return $v;
+            })
+            ->end()
+            ->children()
+            ->scalarNode('service')->defaultNull()->info('Service id to override the retry strategy entirely')->end()
+            ->integerNode('max_retries')->defaultValue(3)->min(0)->end()
+            ->integerNode('delay')->defaultValue(1000)->min(0)->info('Time in ms to delay (or the initial value when multiplier is used)')->end()
+            ->floatNode('multiplier')->defaultValue(2)->min(1)->info('If greater than 1, delay will grow exponentially for each retry: this delay = (delay * (multiple ^ retries))')->end()
+            ->integerNode('max_delay')->defaultValue(0)->min(0)->info('Max time in ms that a retry should ever be delayed (0 = infinite)')->end()
             ->end()
             ->end()
+            ->end()
+            ->end()
+            ->end()
+            ->scalarNode('failure_transport')
+            ->defaultNull()
+            ->info('Transport name to send failed messages to (after all retries have failed).')
             ->end()
             ->scalarNode('default_bus')->defaultNull()->end()
             ->arrayNode('buses')
             ->defaultValue(['messenger.bus.default' => ['default_middleware' => true, 'middleware' => []]])
+            ->normalizeKeys(false)
             ->useAttributeAsKey('name')
             ->arrayPrototype()
             ->addDefaultsIfNotSet()
@@ -120,13 +152,10 @@ final class Configuration implements ConfigurationInterface
             ->defaultTrue()
             ->end()
             ->arrayNode('middleware')
+            ->performNoDeepMerging()
             ->beforeNormalization()
-            ->ifTrue(static function ($v): bool {
-                return \is_string($v) || (\is_array($v) && !\is_int(key($v)));
-            })
-            ->then(static function ($v): array {
-                return [$v];
-            })
+            ->ifTrue(function ($v) { return \is_string($v) || (\is_array($v) && !\is_int(key($v))); })
+            ->then(function ($v) { return [$v]; })
             ->end()
             ->defaultValue([])
             ->arrayPrototype()
@@ -140,12 +169,12 @@ final class Configuration implements ConfigurationInterface
                     return $middleware;
                 }
                 if (1 < \count($middleware)) {
-                    throw new \InvalidArgumentException(sprintf('Invalid middleware at path "framework.messenger": a map with a single factory id as key and its arguments as value was expected, %s given.', \json_encode($middleware)));
+                    throw new \InvalidArgumentException(sprintf('Invalid middleware at path "framework.messenger": a map with a single factory id as key and its arguments as value was expected, %s given.', json_encode($middleware)));
                 }
 
                 return [
-                    'id' => \key($middleware),
-                    'arguments' => \current($middleware),
+                    'id' => key($middleware),
+                    'arguments' => current($middleware),
                 ];
             })
             ->end()
@@ -163,7 +192,10 @@ final class Configuration implements ConfigurationInterface
             ->end()
             ->end()
             ->end()
-            ->end();
+            ->end()
+            ->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
--- src/DependencyInjection/MessengerExtension.php
+++ src/DependencyInjection/MessengerExtension.php
@@ -10,18 +10,39 @@ use Symfony\Component\DependencyInjection\Argument\RewindableGenerator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ChildDefinition;
+use Symfony\Component\DependencyInjection\Compiler\ServiceLocatorTagPass;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBus;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Transport\TransportFactoryInterface;
 use Symfony\Component\Messenger\Transport\TransportInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpTransportFactory;
+use Symfony\Component\Messenger\Bridge\Redis\Transport\RedisTransportFactory;
+use Symfony\Component\Messenger\Bridge\Doctrine\Transport\DoctrineTransportFactory;
 
-final class MessengerExtension extends ConfigurableExtension
+
+final class MessengerExtension extends ConfigurableExtension implements PrependExtensionInterface
 {
+    private $validationConfig = ['enabled' => false];
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $configs = $container->getExtensionConfig('framework');
+        foreach ($configs as $config) {
+            if (isset($config['validation'])) {
+                $this->validationConfig = $config['validation'];
+            }
+        }
+
+    }
+
     protected function loadInternal(array $config, ContainerBuilder $container): void
     {
         $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__) . '/Resources/config'));
@@ -34,13 +55,15 @@ final class MessengerExtension extends ConfigurableExtension
 
         $container->registerForAutoconfiguration(MessageHandlerInterface::class)
             ->addTag('messenger.message_handler');
+
         $container->registerForAutoconfiguration(TransportFactoryInterface::class)
             ->addTag('messenger.transport_factory');
 
-        $this->registerMessengerConfiguration($config, $container);
+        $frameworkConfigs = $container->getExtensionConfig('framework');
+        $this->registerMessengerExtension($config, $container, $this->validationConfig);
     }
 
-    private function registerMessengerConfiguration(array $config, ContainerBuilder $container): void
+    private function OldregisterMessengerConfiguration(array $config, ContainerBuilder $container): void
     {
         if (empty($config['transports'])) {
             $container->removeDefinition('messenger.transport.symfony_serializer');
@@ -63,7 +86,7 @@ final class MessengerExtension extends ConfigurableExtension
         }
 
         $defaultMiddleware = [
-            'before' => [['id' => 'logging']],
+            'before' => [],
             'after' => [['id' => 'send_message'], ['id' => 'handle_message']],
         ];
         foreach ($config['buses'] as $busId => $bus) {
@@ -95,7 +118,7 @@ final class MessengerExtension extends ConfigurableExtension
                 $container->setAlias('message_bus', $busId)->setPublic(true);
                 $container->setAlias(MessageBusInterface::class, $busId);
             } else {
-                $container->registerAliasForArgument($busId, MessageBusInterface::class);
+                $this->registerAliasForArgument($container, $busId, MessageBusInterface::class);
             }
         }
 
@@ -138,8 +161,197 @@ final class MessengerExtension extends ConfigurableExtension
             ->replaceArgument(1, $messagesToSendAndHandle);
     }
 
+    private function registerMessengerExtension(array $config, ContainerBuilder $container, $validationConfig): void
+    {
+        if (!interface_exists(MessageBusInterface::class)) {
+            throw new LogicException('Messenger support cannot be enabled as the Messenger component is not installed. Try running "composer require symfony/messenger".');
+        }
+
+        if (class_exists(AmqpTransportFactory::class)) {
+            $container->getDefinition('messenger.transport.amqp.factory')->addTag('messenger.transport_factory');
+        }
+
+        if (class_exists(RedisTransportFactory::class)) {
+            $container->getDefinition('messenger.transport.redis.factory')->addTag('messenger.transport_factory');
+        }
+
+        if (null === $config['default_bus'] && 1 === \count($config['buses'])) {
+            $config['default_bus'] = key($config['buses']);
+        }
+
+        $defaultMiddleware = [
+            'before' => [
+                ['id' => 'add_bus_name_stamp_middleware'],
+                ['id' => 'reject_redelivered_message_middleware'],
+                ['id' => 'dispatch_after_current_bus'],
+                ['id' => 'failed_message_processing_middleware'],
+            ],
+            'after' => [
+                ['id' => 'send_message'],
+                ['id' => 'handle_message'],
+            ],
+        ];
+        foreach ($config['buses'] as $busId => $bus) {
+            $middleware = $bus['middleware'];
+
+            if ($bus['default_middleware']) {
+                if ('allow_no_handlers' === $bus['default_middleware']) {
+                    $defaultMiddleware['after'][1]['arguments'] = [true];
+                } else {
+                    unset($defaultMiddleware['after'][1]['arguments']);
+                }
+
+                // argument to add_bus_name_stamp_middleware
+                $defaultMiddleware['before'][0]['arguments'] = [$busId];
+
+                $middleware = array_merge($defaultMiddleware['before'], $middleware, $defaultMiddleware['after']);
+            }
+
+            foreach ($middleware as $middlewareItem) {
+                if (!$validationConfig['enabled'] && \in_array($middlewareItem['id'], ['validation', 'messenger.middleware.validation'], true)) {
+                    throw new LogicException('The Validation middleware is only available when the Validator component is installed and enabled. Try running "composer require symfony/validator".');
+                }
+            }
+
+            if ($container->getParameter('kernel.debug') && class_exists(Stopwatch::class)) {
+                array_unshift($middleware, ['id' => 'traceable', 'arguments' => [$busId]]);
+            }
+
+            $container->setParameter($busId.'.middleware', $middleware);
+            $container->register($busId, MessageBus::class)->addArgument([])->addTag('messenger.bus');
+
+            if ($busId === $config['default_bus']) {
+                $container->setAlias('message_bus', $busId)->setPublic(true);
+                $container->setAlias('messenger.default_bus', $busId)->setPublic(true);
+                $container->setAlias(MessageBusInterface::class, $busId);
+            } else {
+                $this->registerAliasForArgument($container, $busId, MessageBusInterface::class);
+            }
+        }
+
+        if (empty($config['transports'])) {
+            $container->removeDefinition('messenger.transport.symfony_serializer');
+            $container->removeDefinition('messenger.transport.amqp.factory');
+            $container->removeDefinition('messenger.transport.redis.factory');
+        } else {
+            $container->getDefinition('messenger.transport.symfony_serializer')
+                      ->replaceArgument(1, $config['serializer']['symfony_serializer']['format'])
+                      ->replaceArgument(2, $config['serializer']['symfony_serializer']['context']);
+            $container->setAlias('messenger.default_serializer', $config['serializer']['default_serializer']);
+        }
+
+        $senderAliases = [];
+        $transportRetryReferences = [];
+        foreach ($config['transports'] as $name => $transport) {
+            $serializerId = $transport['serializer'] ?? 'messenger.default_serializer';
+
+            $transportDefinition = (new Definition(TransportInterface::class))
+                ->setFactory([new Reference('messenger.transport_factory'), 'createTransport'])
+                ->setArguments([$transport['dsn'], $transport['options'] + ['transport_name' => $name], new Reference($serializerId)])
+                ->addTag('messenger.receiver', ['alias' => $name])
+            ;
+            $container->setDefinition($transportId = 'messenger.transport.'.$name, $transportDefinition);
+            $senderAliases[$name] = $transportId;
+
+            if (null !== $transport['retry_strategy']['service']) {
+                $transportRetryReferences[$name] = new Reference($transport['retry_strategy']['service']);
+            } else {
+                $retryServiceId = sprintf('messenger.retry.multiplier_retry_strategy.%s', $name);
+                $retryDefinition = new ChildDefinition('messenger.retry.abstract_multiplier_retry_strategy');
+                $retryDefinition
+                    ->replaceArgument(0, $transport['retry_strategy']['max_retries'])
+                    ->replaceArgument(1, $transport['retry_strategy']['delay'])
+                    ->replaceArgument(2, $transport['retry_strategy']['multiplier'])
+                    ->replaceArgument(3, $transport['retry_strategy']['max_delay']);
+                $container->setDefinition($retryServiceId, $retryDefinition);
+
+                $transportRetryReferences[$name] = new Reference($retryServiceId);
+            }
+        }
+
+        $senderReferences = [];
+        // alias => service_id
+        foreach ($senderAliases as $alias => $serviceId) {
+            $senderReferences[$alias] = new Reference($serviceId);
+        }
+        // service_id => service_id
+        foreach ($senderAliases as $serviceId) {
+            $senderReferences[$serviceId] = new Reference($serviceId);
+        }
+
+        $messageToSendersMapping = [];
+        foreach ($config['routing'] as $message => $messageConfiguration) {
+            if ('*' !== $message && !class_exists($message) && !interface_exists($message, false)) {
+                throw new LogicException(sprintf('Invalid Messenger routing configuration: class or interface "%s" not found.', $message));
+            }
+
+            // make sure senderAliases contains all senders
+            foreach ($messageConfiguration['senders'] as $sender) {
+                if (!isset($senderReferences[$sender])) {
+                    throw new LogicException(sprintf('Invalid Messenger routing configuration: the "%s" class is being routed to a sender called "%s". This is not a valid transport or service id.', $message, $sender));
+                }
+            }
+
+            $messageToSendersMapping[$message] = $messageConfiguration['senders'];
+        }
+
+        $sendersServiceLocator = ServiceLocatorTagPass::register($container, $senderReferences);
+
+        $container->getDefinition('messenger.senders_locator')
+                  ->replaceArgument(0, $messageToSendersMapping)
+                  ->replaceArgument(1, $sendersServiceLocator)
+        ;
+
+        $container->getDefinition('messenger.retry.send_failed_message_for_retry_listener')
+                  ->replaceArgument(0, $sendersServiceLocator)
+        ;
+
+        $container->getDefinition('messenger.retry_strategy_locator')
+                  ->replaceArgument(0, $transportRetryReferences);
+
+        if ($config['failure_transport']) {
+            if (!isset($senderReferences[$config['failure_transport']])) {
+                throw new LogicException(sprintf('Invalid Messenger configuration: the failure transport "%s" is not a valid transport or service id.', $config['failure_transport']));
+            }
+
+            $container->getDefinition('messenger.failure.send_failed_message_to_failure_transport_listener')
+                      ->replaceArgument(0, $senderReferences[$config['failure_transport']]);
+            $container->getDefinition('console.command.messenger_failed_messages_retry')
+                      ->replaceArgument(0, $config['failure_transport']);
+            $container->getDefinition('console.command.messenger_failed_messages_show')
+                      ->replaceArgument(0, $config['failure_transport']);
+            $container->getDefinition('console.command.messenger_failed_messages_remove')
+                      ->replaceArgument(0, $config['failure_transport']);
+        } else {
+            $container->removeDefinition('messenger.failure.send_failed_message_to_failure_transport_listener');
+            $container->removeDefinition('console.command.messenger_failed_messages_retry');
+            $container->removeDefinition('console.command.messenger_failed_messages_show');
+            $container->removeDefinition('console.command.messenger_failed_messages_remove');
+        }
+    }
+
     public function getAlias(): string
     {
         return 'redpill_polyfill_messenger';
     }
+
+    /**
+     * Registers an autowiring alias that only binds to a specific argument name.
+     *
+     * The argument name is derived from $name if provided (from $id otherwise)
+     * using camel case: "foo.bar" or "foo_bar" creates an alias bound to
+     * "$fooBar"-named arguments with $type as type-hint. Such arguments will
+     * receive the service $id when autowiring is used.
+     */
+    public function registerAliasForArgument($container, string $id, string $type, string $name = null)
+    {
+        $name = lcfirst(str_replace(' ', '', ucwords(preg_replace('/[^a-zA-Z0-9\x7f-\xff]++/', ' ', $name ?? $id))));
+
+        if (!preg_match('/^[a-zA-Z_\x7f-\xff]/', $name)) {
+            throw new InvalidArgumentException(sprintf('Invalid argument name "%s" for service "%s": the first character must be a letter.', $name, $id));
+        }
+
+        return $container->setAlias($type.' $'.$name, $id);
+    }
+
 }
